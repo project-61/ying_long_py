3333ee21ff1e85dfbceec0d3810d1b4b8ba8dd9c
imlyzh <enterindex@gmail.com>
2021-11-06 13:02:48 +0800

    update Primary

--- ying_long_py/pin.py
+++ ying_long_py/pin.py
@@ -1,6 +1,4 @@
-
-
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Tuple
 
 from ying_long_py.gen_id import gen_id
 
@@ -9,8 +7,58 @@ class Pin:
     name: str
     in_: Any
     out_: Any
+
     def __init__(self, name: str = gen_id()) -> None:
         self.name = name
+
+    def let(self, in_) -> 'Pin':
+        self.in_=in_
+        return in_
+
+    def get(self, i: int) -> 'Pin':
+        ...
+
+    def get_range(self, l: int, r: int) -> List['Pin']:
+        ...
+
+    def __len__(self) -> int:
+        return 1
+
+    def __or__(self, other: 'Pin') -> 'Pin':
+        return Pin().let(Or(self, other))
+    def __xor__(self, other: 'Pin') -> 'Pin':
+        return Pin().let(Xor(self, other))
+    def __and__(self, other: 'Pin') -> 'Pin':
+        return Pin().let(And(self, other))
+    def __add__(self, other: 'Pin') -> 'Pin':
+        return Pin().let(Add(self, other))
+    def __invert__(self) -> 'Pin':
+        return Pin().let(Invert(self))
+
+
+
+class Primary:
+    ins: List[Any]
+    out_: Any
+    def __init__(self, *args) -> None:
+        self.ins = list(args)
+
+class Or(Primary):
+    def __init__(self, l, r) -> None:
+        super().__init__(l, r)
+class Xor(Primary):
+    def __init__(self, l, r) -> None:
+        super().__init__(l, r)
+class And(Primary):
+    def __init__(self, l, r) -> None:
+        super().__init__(l, r)
+class Add(Primary):
+    def __init__(self, l, r) -> None:
+        super().__init__(l, r)
+
+class Invert(Primary):
+    def __init__(self, l) -> None:
+        super().__init__(l)
 
 
 class Clock(Pin):
